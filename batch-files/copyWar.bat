52cde92ac4322c706718b6e5d8217e55728abf08
Premendra.Kumar <premendra.kumar@indiabulls.com>
2020-01-24 18:05:11 +0530

    Committed for change
    -
    
    Adding updated topic-mgmt db backup-24 Jan 2019

--- batch-files/copyWar.bat
+++ batch-files/copyWar.bat
@@ -1,3 +1,4 @@
+@ECHO off
 SET currentDir=%cd%
 D:
 cd D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\bin\
@@ -6,8 +7,8 @@ call shutdown.bat
 SLEEP 10
 echo sleeping for 10 seconds
 
-copy C:\Users\premendra.kumar\git\bce-gui-30-dec\bce-gui\target\bce-gui.war D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\webapps\bce-gui.war
-copy C:\Users\premendra.kumar\git\bce-rest-services\RestServices\target\RestServices.war D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\webapps\RestServices.war
+::copy C:\Users\premendra.kumar\git\bce-gui-30-dec\bce-gui\target\bce-gui.war D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\webapps\bce-gui.war
+::copy C:\Users\premendra.kumar\git\bce-rest-services\RestServices\target\RestServices.war D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\webapps\RestServices.war
 
 call startup.bat
 cd %currentDir%
