2c7cbab188214c1bad7e1fc5a7a91e638d6da938
Premendra.Kumar <premendra.kumar@indiabulls.com>
2020-01-30 11:16:58 +0530

    Committing changes

--- batch-files/copyWar.bat
+++ batch-files/copyWar.bat
@@ -1,15 +1,17 @@
 @ECHO off
 SET currentDir=%cd%
-D:
-cd D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\bin\
-call shutdown.bat
+::D:
+::cd D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\bin\
+::call shutdown.bat
 
-SLEEP 10
-echo sleeping for 10 seconds
+::SLEEP 10
+::echo sleeping for 10 seconds
 
-::copy C:\Users\premendra.kumar\git\bce-gui-30-dec\bce-gui\target\bce-gui.war D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\webapps\bce-gui.war
-::copy C:\Users\premendra.kumar\git\bce-rest-services\RestServices\target\RestServices.war D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\webapps\RestServices.war
+copy C:\Users\premendra.kumar\git\bce-gui-30-dec\bce-gui\target\bce-gui.war D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\webapps\bce-gui.war
+copy C:\Users\premendra.kumar\git\bce-rest-services\RestServices\target\RestServices.war D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\webapps\RestServices.war
 
-call startup.bat
+copy C:\Users\premendra.kumar\git\db-backup\word-meaning-db-backup\target\word-meaning-db-backup-service.war D:\CUSTOM-INSTALLATIONS\apache-tomcat-8.5.47\webapps\word-meaning-db-backup-service.war
+
+::call startup.bat
 cd %currentDir%
-echo tomcat is being installed
+::echo tomcat is being installed
